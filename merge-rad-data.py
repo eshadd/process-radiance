1ac47ebc3ee1a9183c7fcb1fe8449f52b943a4e9
eshadd <eric@Determinant-LL.Com>
2017-01-29 14:59:27 -0800

    csv and misc

--- merge-rad-data.py
+++ merge-rad-data.py
@@ -4,17 +4,15 @@ import sqlite3
 
 #INPUT
 
-run_name = 'Test 16'
+run_name = 'Prototype 14'
 
-ill_json_fn = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/' + run_name + '/run/0-UserScript/radiance/output/radout.json'
-#n_illmap_xgrid = 15
-#n_illmap_ygrid = 10
-ill_csv_fp = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/' + run_name + '/run/0-UserScript/radiance/output/'
+ill_json_fn = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/' + run_name + '/run/1-UserScript-0/radiance/output/radout.json'
+rad_csv_fp = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/' + run_name + '/run/1-UserScript-0/radiance/output/'
 spc_field = 'T2419_CASE_Spc'
-bad_glare_field = 'Bad_Case_Glare_Sensor'
-good_glare_field = 'Good_Case_Glare_Sensor'
+bad_glare_field = 'Bad_Glare_Snsr'
+good_glare_field = 'Good_Glare_Snsr'
 
-sql = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/' + run_name + '/run/0-UserScript/radiance/sql/eplusout.sql'    
+sql = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/' + run_name + '/run/1-UserScript-0/radiance/sql/eplusout.sql'    
 sql_tbl = 'ReportVariableWithTime '
 sql_field = 'Value'
 sql_filter_col = 'Name'
@@ -34,7 +32,7 @@ sql_crsr = sql_conn.cursor()
 
 #get illuminance map and glare metrics by space
 ill_dat = {}
-illum_hdr = ['bad_dgp', 'bad_raw', 'good_dgp', 'good_raw', 'ill_map']
+illum_hdr = ['bad_dgp', 'bad_raw', 'good_dgp', 'good_raw', 'ext_snsr']
 for hour_dat in ill_d:
     for spc_nm, spc_ill_dat in hour_dat.items():
 
@@ -75,7 +73,7 @@ for spc_nm, spc_ill_dat in ill_dat.items():
 
 #OUTPUT
 
-#for rad_case_nm in rad_case_fields:
-#    with open(rad_csv_fp+rad_case_nm.lower()+'.csv', 'w', newline='') as f_w:
-#        csv.writer(f_w, dialect='excel').writerows(rad_cases_a_by_d[rad_case_nm])
+for spc_nm, spc_dat in rad_dat.items():
+    with open(rad_csv_fp+spc_nm.lower()+'.csv', 'w', newline='') as f_w:
+        csv.writer(f_w, dialect='excel').writerows(spc_dat)
 
