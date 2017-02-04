ea0dac1c0207f52f9fde1489da23289e57af4a90
eshadd <eric@Determinant-LL.Com>
2017-02-04 12:19:46 -0800

    loop by separate shade config runs

---
+++ distill_rad_data.py
@@ -0,0 +1,133 @@
+import json
+import csv
+import sqlite3
+
+def run_distillr(run_name, analysis_path):
+
+    #INPUT
+
+    ill_json_fn = analysis_path + run_name + '/run/1-UserScript-0/radiance/output/radout.json'
+
+    bad_glare_field = 'Bad_Glare_Snsr'
+    #good_glare_field = 'Good_Glare_Snsr'
+    bad_glare_threshold = 0.4
+    good_glare_threshold = 0.6
+    bad_min_shade_period = 21*24
+    good_min_shade_period = 1
+    bad_shade_check_times = [8]
+    good_shade_check_times = [8, 12]
+    bad_occ_start = 17
+    good_occ_start = 17
+    bad_occ_end = 17
+    good_occ_end = 17
+
+    sql = analysis_path + run_name + '/run/1-UserScript-0/radiance/sql/eplusout.sql'    
+    sql_tbl = 'ReportVariableWithTime '
+    sql_field = 'Value'
+    sql_filter_col = 'Name'
+    sql_filter_a = ['Site Solar Azimuth Angle', 'Site Solar Altitude Angle', 'Site Sky Diffuse Solar Radiation Luminous Efficacy', 'Site Beam Solar Radiation Luminous Efficacy']
+    sql_time = 'TimeIndex, Month, Day, Hour, DayType'
+
+    #SETUP
+
+    with open(ill_json_fn) as ill_f:
+        ill_raw_d = json.load(ill_f)
+        ill_raw_a = ill_raw_d.pop('all_hours')
+
+    #reformat so main loop can loop by space.
+    ill_d = {spc_nm: [] for spc_nm in ill_raw_a[0].keys()}
+    for hr_dat in ill_raw_a:
+         for spc_nm, spc_dat in hr_dat.items():
+            ill_d[spc_nm].append([spc_dat[0], spc_dat[2]])
+
+    # Connect to the database file
+    sql_conn = sqlite3.connect(sql)
+    sql_crsr = sql_conn.cursor()
+
+    #MAIN
+
+    #get solar data
+    solar = []
+    sql_crsr.execute('SELECT {tm} FROM {tn} WHERE {cn}="{ft}"'.format(tm=sql_time,tn=sql_tbl,cn=sql_filter_col,ft=sql_filter_a[0]))
+    solar.append(sql_crsr.fetchall())
+    for sql_filter in sql_filter_a:
+        sql_crsr.execute('SELECT {fd} FROM {tn} WHERE {cn}="{ft}"'.format(fd=sql_field,tn=sql_tbl,cn=sql_filter_col,ft=sql_filter))
+        solar.append(sql_crsr.fetchall())
+    sql_conn.close()
+
+    #get illuminance map and glare metrics by space
+    ill_dat = []
+    for spc_nm, spc_ill_dat in ill_d.items():
+
+        form_dat = spc_nm.split('_')
+
+        bad_shaded_hrs = 0
+        good_shaded_hrs = 0
+        bad_shaded = 0
+        good_shaded = 0
+        for hr, hour_dat in enumerate(spc_ill_dat):
+
+            grid_ill_dat = hour_dat[0]
+            sensor_glare_dat = hour_dat[1]
+        
+            glare_field_a = list(sensor_glare_dat.keys())
+            bad_glare_idx = glare_field_a.index([bs for bs in list(sensor_glare_dat.keys()) if bad_glare_field in bs][0])
+            bad_glare = sensor_glare_dat[glare_field_a[bad_glare_idx]]
+            good_glare = sensor_glare_dat[glare_field_a[int(not bad_glare_idx)]]
+        
+            #assumes only one view
+            bad_glare_metrics = bad_glare[list(bad_glare.keys())[0]]
+            good_glare_metrics = good_glare[list(good_glare.keys())[0]]
+
+            bad_dgp = bad_glare_metrics['dgp']
+            good_dgp = good_glare_metrics['dgp']
+
+            day_type = solar[0][hr][4]
+            time_of_day = solar[0][hr][3]
+
+            if time_of_day >= bad_occ_start and time_of_day <= bad_occ_end:
+                if bad_dgp > bad_glare_threshold:
+                    bad_shaded = 1
+                    bad_shaded_hrs += 1
+                else:
+                    #if not glarey now, then if unshaded on last loop, or shaded but is now time to check...
+                    if bad_shaded == 0 or (bad_shaded_hrs >= bad_min_shade_period and day_type not in ['Saturday', 'Sunday', 'Holiday'] and time_of_day in bad_shade_check_times):
+                        bad_shaded = 0
+                        bad_shaded_hrs = 0
+                    else:
+                        bad_shaded = 1
+                        bad_shaded_hrs += 1
+            else:
+                if bad_shaded == 1:
+                    bad_shaded = 1
+                    bad_shaded_hrs +=1
+                else:
+                    bad_shaded = 0
+                    bad_shaded_hrs = 0
+
+            if time_of_day >= good_occ_start and time_of_day <= good_occ_end:
+                if good_dgp > good_glare_threshold:
+                    good_shaded = 1
+                    good_shaded_hrs += 1
+                else:
+                    #if not glarey now, then if unshaded on last loop, or shaded but is now time to check...
+                    if good_shaded == 0 or (good_shaded_hrs >= good_min_shade_period and day_type not in ['Saturday', 'Sunday', 'Holiday'] and time_of_day in good_shade_check_times):
+                        good_shaded = 0
+                        good_shaded_hrs = 0
+                    else:
+                        good_shaded = 1
+                        good_shaded_hrs += 1
+            else:
+                if bad_shaded == 1:
+                    bad_shaded = 1
+                    bad_shaded_hrs +=1
+                else:
+                    bad_shaded = 0
+                    bad_shaded_hrs = 0
+
+            hour_ill_dat = [int(form_dat[0]), int(form_dat[1]), solar[0][hr][0], solar[0][hr][1], solar[0][hr][2], time_of_day, day_type, 
+                            bad_dgp, good_dgp, bad_shaded, good_shaded, grid_ill_dat[0], grid_ill_dat[13]]
+
+            ill_dat.append(hour_ill_dat)
+
+    return ill_dat
--- run_parser.py
+++ run_parser.py
@@ -1,7 +1,8 @@
 import os
+import distill_rad_data as distilry
 
-run_folder_parent_path = analysis_path
-out_folder_path = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/Output/'
+analysis_path = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/'
+out_folder_path = analysis_path + '/Output/'
 az_set_a = [[90, 180, 270], [90, 135, 180, 235, 270]]
 view_config_a = ['WN', 'SL']
 clrstry_config_a = ['NC', 'WN', 'LL', '3M']
@@ -11,15 +12,29 @@ cz_a = [3, 7, 12, 14, 16]
 def join_fmtd_a_nums(array):
     return ' '.join([format(num, '03d') for num in array])
 
+ill_by_shd = []
 for az_set in az_set_a:
+
     az_set_fmtd = join_fmtd_a_nums(az_set)
+    ill_by_config = []
+    
     for view_config in view_config_a:
-        for shade_config in shade_config_a:
-            for clrstry_config in clrstry_config_a:
-                for shade_config in shade_config_a:
-                    for cz in cz_a:
-                        run_nm = ' '.join([az_set_fmtd, view_config, shade_config, clrstry_config, shade_config, format(cz, '02d')])
-                        if os.path.isdir(run_folder_parent_path + run_nm):
-                            subfolder = view_config + ' ' + clrstry_config
-                            print(run_folder)
+        for clrstry_config in clrstry_config_a:
+            for wn_shade_config in shade_config_a:
+                
+                ill_by_config.extend(ill_by_shd)
+                ill_by_shd = []
+                run_nm = ' '.join([az_set_fmtd, view_config, wn_shade_config, clrstry_config, wn_shade_config])
+
+                for cz in cz_a:
+
+                    run_nm = run_nm + ' ' + format(cz, '02d')
+                    if os.path.isdir(analysis_path + run_nm):
+                        ill_by_shd.append(distilry.run_distillr(run_nm, analysis_path))
+
+#OUTPUT
+
+for spc_nm, spc_dat in ill_dat.items():
+    with open(rad_csv_fp+run_name+'_'+spc_nm.lower()+'.csv', 'w', newline='') as f_w:
+        csv.writer(f_w, dialect='excel').writerows(spc_dat)
 
