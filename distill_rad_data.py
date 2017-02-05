6b67f76f00d557dffe1c44f1144b5548ca0d2547
eshadd <eric@Determinant-LL.Com>
2017-02-05 14:42:25 -0800

    alpha

--- distill_rad_data.py
+++ distill_rad_data.py
@@ -1,7 +1,7 @@
 import json
 import sqlite3
 
-def run_distillr(run_name, analysis_path):
+def run_distillr(run_name, climate_zone, view_config, clrstry_config, analysis_path):
 
     #INPUT
 
@@ -55,7 +55,7 @@ def run_distillr(run_name, analysis_path):
     sql_conn.close()
 
     #get illuminance map and glare metrics by space
-    ill_dat = [['Az', 'WWR', 'Ann Hr', 'Month', 'Day', 'Hr', 'Day Type', 'Bad DGP', 'Good DGP', 'Bad Shaded?', 'Good Shaded?', '1ry DS', '2ry DS']]
+    ill_dat = [['CZ', 'View', 'Clrstry', 'Az', 'WWR', 'Ann Hr', 'Month', 'Day', 'Hr', 'Day Type', 'Bad DGP', 'Good DGP', 'Bad Shaded?', 'Good Shaded?', '1ry DS', '2ry DS']]
     for spc_nm, spc_ill_dat in ill_d.items():
 
         form_dat = spc_nm.split('_')
@@ -124,7 +124,7 @@ def run_distillr(run_name, analysis_path):
                     bad_shaded = 0
                     bad_shaded_hrs = 0
 
-            hr_ill_dat = [int(form_dat[0]), int(form_dat[1]), solar[0][hr][0], solar[0][hr][1], solar[0][hr][2], time_of_day, day_type, 
+            hr_ill_dat = [climate_zone, view_config, clrstry_config, int(form_dat[0]), int(form_dat[1]), solar[0][hr][0], solar[0][hr][1], solar[0][hr][2], time_of_day, day_type, 
                             bad_dgp, good_dgp, bad_shaded, good_shaded, grid_ill_dat[0], grid_ill_dat[13]]
 
             ill_dat.append(hr_ill_dat)
--- run_parser.py
+++ run_parser.py
@@ -3,41 +3,44 @@ import csv
 
 import distill_rad_data as distilry
 
+#INPUTS 
+
 analysis_path = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/'
 out_path = analysis_path + '/Output/'
 az_set_a = [[90, 180, 270], [90, 135, 180, 235, 270]]
 view_config_a = ['WN', 'SL']
 clrstry_config_a = ['NC', 'WN', 'LL', '3M']
-shade_config_a = ['NS', 'LS', 'DS']
+shd_type_a = ['NS', 'LS', 'DS']
 cz_a = [3, 7, 12, 14, 16]
+
+#SETUP
 
 def join_fmtd_a_nums(array):
     return ' '.join([format(num, '03d') for num in array])
 
-ill_by_shd = []
-for az_set in az_set_a:
+#MAIN
 
-    az_set_fmtd = join_fmtd_a_nums(az_set)
-    ill_by_config = []
-    
-    for view_config in view_config_a:
-        for clrstry_config in clrstry_config_a:
-            for wn_shade_config in shade_config_a:
-                run_nm = ' '.join([az_set_fmtd, view_config, wn_shade_config, clrstry_config, wn_shade_config])
-                has_ill_dat = False
-                for cz in cz_a:
+for cz in cz_a:
+    for view_shd_type in shd_type_a:
 
-                    run_nm = run_nm + ' ' + format(cz, '02d')
-                    if os.path.isdir(analysis_path + run_nm):
-                        ill_by_shd.extend(distilry.run_distillr(run_nm, analysis_path))
-                        has_ill_dat = True
-                    run_nm = run_nm[:-3]
+        #this is the output file array.
+        ill_dat = []
 
-                #OUTPUT
+        for view_config in view_config_a:
+            for clrstry_shd_type in shd_type_a:
+                for clrstry_config in clrstry_config_a:
+                    for az_set in az_set_a:
 
-                if has_ill_dat:
-                    ill_dir = out_path + view_config + ' ' + clrstry_config + '/'
-                    if not os.path.exists(ill_dir):
-                        os.makedirs(ill_dir)
-                    with open(ill_dir + wn_shade_config + '.csv', 'w', newline='') as f_w:
-                        csv.writer(f_w, dialect='excel').writerows(ill_by_shd)
+                        az_set_fmtd = join_fmtd_a_nums(az_set)                   
+                        run_nm = ' '.join([az_set_fmtd, view_config, view_shd_type, clrstry_config, clrstry_shd_type, format(cz, '02d')])
+
+                        if os.path.isdir(analysis_path + run_nm):
+                            ill_dat.extend(distilry.run_distillr(run_nm, cz, view_config, clrstry_config, analysis_path))
+
+            #OUTPUT
+
+            if ill_dat:
+                if not os.path.exists(out_path):
+                    os.makedirs(out_path)
+                with open(out_path + ' '.join([format(cz, '02d'), view_shd_type]) + '.csv', 'w', newline='') as f_w:
+                    csv.writer(f_w, dialect='excel').writerows(ill_dat)
