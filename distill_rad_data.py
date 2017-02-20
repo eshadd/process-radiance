4f06ba93176fb7ff355460378af90f9c02d083ac
eshadd <eric@Determinant-LL.Com>
2017-02-20 14:38:17 -0800

    good shade fr run

--- distill_rad_data.py
+++ distill_rad_data.py
@@ -4,7 +4,7 @@ import sqlite3
 import os
 import bisect as bs
 
-def run_distillr(run_name, setpt_a, shade_case_d, out_path, wthr_fn, tdv_fref):
+def run_distillr(case, setpt_a, shade_case_d, run_path_a, wthr_fn, tdv_fref):
 
     #INPUT
 
@@ -12,22 +12,6 @@ def run_distillr(run_name, setpt_a, shade_case_d, out_path, wthr_fn, tdv_fref):
     nmap_hdr_rows = 4
     ntdv_hdr_rows = 3
     
-    #bad_glare_threshold = 0.4
-    #good_glare_threshold = 0.6
-    #bad_min_shade_period = 21*24
-    #good_min_shade_period = 1
-    #bad_shade_check_times = [8]
-    #good_shade_check_times = [8, 12]
-    #bad_occ_start = 8
-    #good_occ_start = 8
-    #bad_occ_end = 17
-    #good_occ_end = 17
-
-    #bad_shaded = 0
-    #bad_shaded_hrs = 0
-    #good_shaded = 0
-    #good_shaded_hrs = 0
-
     day_type_a = ['Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed'] 
 
     min_lamp_pwr = 0.2
@@ -47,14 +31,20 @@ def run_distillr(run_name, setpt_a, shade_case_d, out_path, wthr_fn, tdv_fref):
     
     #SETUP
 
-    config = run_name.split(' ')
-    wn_case = config[0]
-    wn_shd = config[1] if len(config) > 1 else 'NS'
-    rad_set = [wthr_fn, wn_case, wn_shd]
-    
-    ill_dir_a = [ref for ref in os.walk(out_path + 'ts/')][2::2]
-    ill_fref_a = [[fn_a[0], path + '/' + fn_a[0], path + '/' + fn_a[1]] for path, blank, fn_a in ill_dir_a]
+    ill_dir_d = {}
+    for shd_case, run_path in run_path_a.items():
 
+        dat_pt = run_path.split('/')[-1]
+        run_idx = int(dat_pt.replace('dataPoint', ''))
+        rad_out_path = run_path + '/' + str(9 * (run_idx - 1)) + '-Userscript-0/radiance/output/'
+
+        ill_dir_d[shd_case] = [ref for ref in os.walk(rad_out_path + 'ts/')][2::2]
+
+    ill_fref_a = [[fn_a[0], path + '/' + fn_a[0], path + '/' + fn_a[1]] for path, blank, fn_a in ill_dir_d['none']]
+    ill_fref_a = [nsfref + [ill_dir_d['good'][spc_idx][0] + '/' + ill_dir_d['good'][spc_idx][2][1]] for spc_idx, nsfref in enumerate(ill_fref_a)]
+
+    rad_set = [wthr_fn, case]
+    
     cz_spos = wthr_fn.find('CZ') + 2
     cz = int(wthr_fn[cz_spos:cz_spos+2])
 
@@ -63,7 +53,7 @@ def run_distillr(run_name, setpt_a, shade_case_d, out_path, wthr_fn, tdv_fref):
     #Loop through radiance data files
     rad_dat = []
     rad_hr_dat = []
-    for glr_fnm, glr_fref, map_fref in ill_fref_a:        
+    for glr_fnm, glr_fref, map_fref, gd_shd_map_fref in ill_fref_a:        
 
         spc_form_a = glr_fnm[:-3].split('_')
         spc_az = int(spc_form_a[0])
@@ -73,127 +63,149 @@ def run_distillr(run_name, setpt_a, shade_case_d, out_path, wthr_fn, tdv_fref):
             glr_rdr = csv.reader(csvfile)
             for hdr_rw in range(nglr_hdr_rows):
                 next(glr_rdr)
+
             with open(map_fref) as fr:
                 map_rdr = csv.reader(fr)
                 for hdr_rw in range(nmap_hdr_rows):
                     next(map_rdr)
                 
-                with open(tdv_fref) as fr:
-                    tdv_rdr = csv.reader(fr)
-                    for hdr_rw in range(ntdv_hdr_rows):
-                        next(tdv_rdr)
+                with open(gd_shd_map_fref) as fr:
+                    gd_shd_map_rdr = csv.reader(fr)
+                    for hdr_rw in range(nmap_hdr_rows):
+                        next(gd_shd_map_rdr)
                 
-                    #init blinds/shades
-                    for case in ('bad', 'good'):
-                        shade_case_d[case]['shaded'] = 0
-                        shade_case_d[case]['shaded_hrs'] = 0
+                    with open(tdv_fref) as fr:
+                        tdv_rdr = csv.reader(fr)
+                        for hdr_rw in range(ntdv_hdr_rows):
+                            next(tdv_rdr)
+                
+                        #init blinds/shades
+                        for case in ('bad', 'good'):
+                            shade_case_d[case]['shaded'] = 0
+                            shade_case_d[case]['shaded_hrs'] = 0
 
-                    #aggregate data
-                    for ann_hr_idx, glr_rw in enumerate(glr_rdr):
-                        ann_hr = ann_hr_idx + 1
+                        #aggregate data
+                        for ann_hr_idx, glr_rw in enumerate(glr_rdr):
+                            ann_hr = ann_hr_idx + 1
 
-                        #init ctrl TDV
-                        ctrl_init = [0 for sp in setpt_a]
-                        pdim_a = ctrl_init
-                        sdim_a = ctrl_init
-                        pmulti_a = ctrl_init
-                        smulti_a = ctrl_init
-                        pbi_a = ctrl_init
-                        sbi_a = ctrl_init
+                            #init ctrl TDV
+                            ctrl_init = [0 for sp in setpt_a]
+                            pdim_a = ctrl_init
+                            sdim_a = ctrl_init
+                            pmulti_a = ctrl_init
+                            smulti_a = ctrl_init
+                            pbi_a = ctrl_init
+                            sbi_a = ctrl_init
 
-                        #weather, configuration
-                        rad_hr_dat.extend(rad_set)
-                        rad_hr_dat.append(spc_az)
-                        rad_hr_dat.append(spc_wwr)
+                            #weather, configuration
+                            rad_hr_dat.extend(rad_set)
+                            rad_hr_dat.append(spc_az)
+                            rad_hr_dat.append(spc_wwr)
 
-                        rad_hr_dat.append(ann_hr)
-                        day_type = day_type_a[int(ann_hr/24) % 7]
-                        rad_hr_dat.append(day_type)
+                            rad_hr_dat.append(ann_hr)
+                            day_type = day_type_a[int(ann_hr/24) % 7]
+                            rad_hr_dat.append(day_type)
 
-                        #sensors
-                        map_a = next(map_rdr)
-                        prim_zn_ill = float(map_a[6])
-                        secd_zn_ill = float(map_a[-1])
-                        rad_hr_dat.append(prim_zn_ill)
-                        rad_hr_dat.append(secd_zn_ill)
+                            #sensors
+                            map_a = next(map_rdr)
+                            prim_zn_ill = float(map_a[6])
+                            secd_zn_ill = float(map_a[-1])
+                            rad_hr_dat.append(prim_zn_ill)
+                            rad_hr_dat.append(secd_zn_ill)
 
-                        #glare
-                        rad_hr_dat.extend(glr_rw[:5])
+                            gd_shd_map = next(gd_shd_map_rdr)
+                            gd_shd_pzn_ill = float(gd_shd_map[6])
+                            gd_shd_szn_ill = float(gd_shd_map[-1])
 
-                        time_of_day = int(glr_rw[2].replace(':00:00',''))
-                        dgp = float(glr_rw[4])
+                            #glare
+                            rad_hr_dat.extend(glr_rw[:5])
 
-                        #tdv
-                        ltg_sched = ltg_sched_a[day_type_ltg_sched[day_type]]
-                        tdv = float(next(tdv_rdr)[cz]) * ltg_sched[time_of_day - 1]
-                        rad_hr_dat.append(tdv)
+                            time_of_day = int(glr_rw[2].replace(':00:00',''))
+                            dgp = float(glr_rw[4])
 
-                        for case in ('bad', 'good'): #not dict iter so preserve this order.
+                            #tdv
+                            ltg_sched = ltg_sched_a[day_type_ltg_sched[day_type]]
+                            tdv = float(next(tdv_rdr)[cz]) * ltg_sched[time_of_day - 1]
+                            rad_hr_dat.append(tdv)
 
-                            shading = shade_case_d[case]
+                            for case in ('bad', 'good'): #not dict iter so preserve this order.
 
-                            if time_of_day >= shading['occ_hrs'][0] and time_of_day <= shading['occ_hrs'][1]:
-                                if dgp > shading['threshold']:
-                                    shading['shaded'] = 1
-                                    shading['shaded_hrs'] += 1
-                                    pzn_ill = 0.0
-                                    szn_ill = 0.0
+                                shading = shade_case_d[case]
+
+                                if time_of_day >= shading['occ_hrs'][0] and time_of_day <= shading['occ_hrs'][1]:
+                                    if dgp > shading['threshold']:
+                                        shading['shaded'] = 1
+                                        shading['shaded_hrs'] += 1
+                                        if case == 'bad':
+                                            pzn_ill = 0.0
+                                            szn_ill = 0.0
+                                        else:
+                                            pzn_ill = gd_shd_pzn_ill
+                                            szn_ill = gd_shd_szn_ill
+                                    else:
+                                        #if not glarey now, then if unshaded on last loop, or shaded but is now time to check...
+                                        if shading['shaded'] == 0 or (shading['shaded_hrs'] >= shading['min_period'] and day_type not in ['Sat', 'Sun', 'Hol'] and time_of_day in shading['check_times']):
+                                            shading['shaded'] = 0
+                                            shading['shaded_hrs'] = 0
+                                            pzn_ill = prim_zn_ill
+                                            szn_ill = secd_zn_ill
+                                        else:
+                                            shading['shaded'] = 1
+                                            shading['shaded_hrs'] += 1
+                                            if case == 'bad':
+                                                pzn_ill = 0.0
+                                                szn_ill = 0.0
+                                            else:
+                                                pzn_ill = gd_shd_pzn_ill
+                                                szn_ill = gd_shd_szn_ill
                                 else:
-                                    #if not glarey now, then if unshaded on last loop, or shaded but is now time to check...
-                                    if shading['shaded'] == 0 or (shading['shaded_hrs'] >= shading['min_period'] and day_type not in ['Sat', 'Sun', 'Hol'] and time_of_day in shading['check_times']):
+                                    if shading['shaded'] == 1:
+                                        shading['shaded_hrs'] +=1
+                                        if case == 'bad':
+                                            pzn_ill = 0.0
+                                            szn_ill = 0.0
+                                        else:
+                                            pzn_ill = gd_shd_pzn_ill
+                                            szn_ill = gd_shd_szn_ill
+                                    else:
                                         shading['shaded'] = 0
                                         shading['shaded_hrs'] = 0
                                         pzn_ill = prim_zn_ill
                                         szn_ill = secd_zn_ill
-                                    else:
-                                        shading['shaded'] = 1
-                                        shading['shaded_hrs'] += 1
-                                        pzn_ill = 0.0
-                                        szn_ill = 0.0
-                            else:
-                                if shading['shaded'] == 1:
-                                    shading['shaded_hrs'] +=1
-                                    pzn_ill = 0.0
-                                    szn_ill = 0.0
-                                else:
-                                    shading['shaded'] = 0
-                                    shading['shaded_hrs'] = 0
-                                    pzn_ill = prim_zn_ill
-                                    szn_ill = secd_zn_ill
 
-                            rad_hr_dat.append(shading['shaded'])
+                                rad_hr_dat.append(shading['shaded'])
 
-                            # power limits
-                            pzn_rat_a = [max(min(1 - pwr_slope * pzn_ill/sp, 1), min_lamp_pwr) for sp in setpt_a]
-                            szn_rat_a = [max(min(1 - pwr_slope * szn_ill/sp, 1), min_lamp_pwr) for sp in setpt_a]
+                                # power limits
+                                pzn_rat_a = [max(min(1 - pwr_slope * pzn_ill/sp, 1), min_lamp_pwr) for sp in setpt_a]
+                                szn_rat_a = [max(min(1 - pwr_slope * szn_ill/sp, 1), min_lamp_pwr) for sp in setpt_a]
 
-                            #the below control calcs use the average of the cases.
+                                #the below control calcs use the average of the cases.
                             
-                            # dimming
-                            pdim_a = [pdim_a[idx] + curr for idx, curr in enumerate([rat * tdv/2 for rat in pzn_rat_a])]
-                            sdim_a = [sdim_a[idx] + curr for idx, curr in enumerate([rat * tdv/2 for rat in szn_rat_a])]
+                                # dimming
+                                pdim_a = [pdim_a[idx] + curr for idx, curr in enumerate([rat * tdv/2 for rat in pzn_rat_a])]
+                                sdim_a = [sdim_a[idx] + curr for idx, curr in enumerate([rat * tdv/2 for rat in szn_rat_a])]
 
-                            # multi-level
-                            pmulti_a = [pmulti_a[idx] + curr for idx, curr in enumerate([multi_level_a[bs.bisect_left(multi_level_a, rat)] * tdv/2 for rat in pzn_rat_a])]
-                            smulti_a = [smulti_a[idx] + curr for idx, curr in enumerate([multi_level_a[bs.bisect_left(multi_level_a, rat)] * tdv/2 for rat in szn_rat_a])]
+                                # multi-level
+                                pmulti_a = [pmulti_a[idx] + curr for idx, curr in enumerate([multi_level_a[bs.bisect_left(multi_level_a, rat)] * tdv/2 for rat in pzn_rat_a])]
+                                smulti_a = [smulti_a[idx] + curr for idx, curr in enumerate([multi_level_a[bs.bisect_left(multi_level_a, rat)] * tdv/2 for rat in szn_rat_a])]
 
-                            # bi-level
-                            pbi_a = [pbi_a[idx] + curr for idx, curr in enumerate([bi_level_a[bs.bisect_left(bi_level_a, rat)] * tdv/2 for rat in pzn_rat_a])]
-                            sbi_a = [sbi_a[idx] + curr for idx, curr in enumerate([bi_level_a[bs.bisect_left(bi_level_a, rat)] * tdv/2 for rat in szn_rat_a])]
+                                # bi-level
+                                pbi_a = [pbi_a[idx] + curr for idx, curr in enumerate([bi_level_a[bs.bisect_left(bi_level_a, rat)] * tdv/2 for rat in pzn_rat_a])]
+                                sbi_a = [sbi_a[idx] + curr for idx, curr in enumerate([bi_level_a[bs.bisect_left(bi_level_a, rat)] * tdv/2 for rat in szn_rat_a])]
 
-                        #include averaged
-                        rad_hr_dat.extend(pdim_a)
-                        rad_hr_dat.extend(sdim_a)
-                        rad_hr_dat.extend(pmulti_a)
-                        rad_hr_dat.extend(smulti_a)
-                        rad_hr_dat.extend(pbi_a)
-                        rad_hr_dat.extend(sbi_a)
+                            #include averaged
+                            rad_hr_dat.extend(pdim_a)
+                            rad_hr_dat.extend(sdim_a)
+                            rad_hr_dat.extend(pmulti_a)
+                            rad_hr_dat.extend(smulti_a)
+                            rad_hr_dat.extend(pbi_a)
+                            rad_hr_dat.extend(sbi_a)
 
-                        #append and reset
-                        rad_dat.append(rad_hr_dat)
-                        rad_hr_dat = []
+                            #append and reset
+                            rad_dat.append(rad_hr_dat)
+                            rad_hr_dat = []
 
-    return (wn_shd, rad_dat)
+    return (cz, rad_dat)
     
 def run_distillr_expanded(run_name, climate_zone, view_config, clrstry_config, analysis_path):
 
--- process_pat.py
+++ process_pat.py
@@ -8,37 +8,43 @@ import os
 
 #INPUT
 
-pat_path = 'E:/05 to 01 Shades - 12/'
+pat_path = 'E:/05 to 01 Shades - 03/'
 out_path = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/Output/'
 tdv_fref = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/Input/2019 TDV Factors-V2-02.13.2017 - NRE30.csv'
 
-setpt_a = [50,  200, 300, 500, 750, 1000]
+tech_case_a = ['WN', '1.75SL', '2.0SL', '2.5SL', '3.0SL']
 
 shade_case_d = {
     'bad': {'threshold': 0.4, 'min_period': 21*24, 'check_times': [8], 'occ_hrs': [8, 17], 'shaded': 0, 'shaded_hrs': 0, \
-        'shd_run': 0.0, 'pzn_ill': 0, 'szn_ill': 0}, 
+        'unshd_run': 'NS', 'shd_run': 0.0}, 
     'good': {'threshold': 0.6, 'min_period': 1, 'check_times': [8, 12], 'occ_hrs': [8, 17], 'shaded': 0, 'shaded_hrs': 0, \
-        'shd_run': 'DS', 'pzn_ill': 0, 'szn_ill': 0}
+        'unshd_run': 'NS', 'shd_run': 'DS'}
     }
 
-rad_dat_hdr = ['Weather', 'Case', 'Shade', 'Az', 'WWR', 'Ann Hr', 'Day Type', \
-    '1ry Daylt', '2ry Daylt', 'Month', 'Day', 'Hr', 'Sensor', 'DGP', 'TDV', 'Bad Shaded?', 'Good Shaded?']
+setpt_a = [50,  200, 300, 500, 750, 1000]
+
+rad_dat_hdr = ['Weather', 'Case', 'Az', 'WWR', 'Ann Hr', 'Day Type', \
+    '1ry Daylt', '2ry Daylt', 'Month', 'Day', 'Hr', 'Sensor', 'DGP', 'TDV', 'Bad Shaded?','Good Shaded?']
 
 pat_db = 'project.osp'
 
 #SETUP
 
-case_hdr_a = []
-for zn in [' 1 ', ' 2 ']:
-    for ctrl in ['Dim', 'Multi', 'Bi']:
-        case_hdr_a.extend([str(sp) + zn + ctrl for sp in setpt_a])
+case_d = {tc: {'none': tc + ' NS', 'good': tc + ' DS'} for tc in tech_case_a}
+
+ctrl_hdr_a = []
+for ctrl in ['Dim', 'Multi', 'Bi']:
+    for zn in [' 1 ', ' 2 ']:
+        ctrl_hdr_a.extend([str(sp) + zn + ctrl for sp in setpt_a])
 
 # Connect to the database file
 pat_db_conn = sqlite3.connect(pat_path + pat_db)
 pat_db_crsr = pat_db_conn.cursor()
 
 pat_db_crsr.execute('SELECT name, directory FROM DataPointRecords')
-run_path_a = pat_db_crsr.fetchall()
+#note inconsitency fix: some no shade cases had 'NS', some didn't
+all_run_path_d = {rn if rn[-3]==' ' else rn + ' NS': rp for rn, rp in pat_db_crsr.fetchall()}
+run_path_d = {case: {'none': all_run_path_d[runs['none']], 'good': all_run_path_d[runs['good']]} for case, runs in case_d.items()}
 
 pat_db_crsr.execute('SELECT weatherFileReferenceRecordId FROM AnalysisRecords')
 wthr_file_idx = pat_db_crsr.fetchone()[0]
@@ -48,17 +54,13 @@ wthr_fn = pat_db_crsr.fetchone()[0]
 pat_db_conn.close()
 
 results_by_shd = {}
-for run_name, run_path in run_path_a:
-    dat_pt = run_path.split('/')[-1]
-    run_idx = int(dat_pt.replace('dataPoint', ''))
-    rad_out_path = pat_path + dat_pt + '/' + str(9 * (run_idx - 1)) + '-Userscript-0/radiance/output/'
-    if os.path.exists(rad_out_path):
-        rad_dat = distill_rad_data.run_distillr(run_name, setpt_a, shade_case_d, rad_out_path, wthr_fn, tdv_fref)
-        results_by_shd.setdefault(rad_dat[0], [rad_dat_hdr + case_hdr_a]).extend(rad_dat[1])
+for case, run_path_d in run_path_d.items():
+    rad_dat = distill_rad_data.run_distillr(case, setpt_a, shade_case_d, run_path_d, wthr_fn, tdv_fref)
+    results_by_shd.setdefault(rad_dat[0], [rad_dat_hdr + ctrl_hdr_a]).extend(rad_dat[1])
 
-for wn_shd, results in results_by_shd.items():
+for cz, results in results_by_shd.items():
     if not os.path.exists(out_path):
         os.makedirs(out_path)
-    with open(out_path + wn_shd + '.csv', 'w', newline='') as f_w:
+    with open(out_path + format(cz, '02d') + '.csv', 'w', newline='') as f_w:
         csv.writer(f_w, dialect='excel').writerows(results)
 
