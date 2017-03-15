a7a8b45a83f4233a30ca7b08f3faf49ec132ac63
eshadd <eric@Determinant-LL.Com>
2017-03-15 16:20:13 -0700

    beta

--- glare_metrics_via_hdr.py
+++ glare_metrics_via_hdr.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import os
+from  multiprocessing import pool
 import csv
 
 #INPUT
@@ -13,20 +14,22 @@ day_type_a = ['Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed']
 #rpict specs
 color = '-av .3 .3 .3'
 accuracy = '-ab 2 -ad 512 -as 256 -dj 1 -dp 1 -dt 0 -dc 1 -lw 0.001'
-res = '-x 100 -y 100'
+res = '-x 50 -y 50'
 
 #model specs
-az_lbls = ['090']#, '180', '270']
-wwrs = ['20']#, '20', '30', '40']
+az_lbl_a = ['090', '180', '270']
+wwr_a = ['10', '20', '30', '40']
 sensr_tag = '_Wn_Spc_Bad_Glare_Snsr_gs.vfv'
 
 #radiance specs
 rad_dir = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/090 180 270 WN UN NC NS/run/1-UserScript-0/radiance'
 sky_scene_dir = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/Input/Weather/'
-sky_scene_roots = ['CZ12']#, 'CZ02', 'CZ03', 'CZ04', \
-                   #'CZ05', 'CZ06', 'CZ07', 'CZ08', \
-                   #'CZ09', 'CZ10', 'CZ11', 'CZ12', \
-                   #'CZ13', 'CZ14', 'CZ15','CZ16']
+sky_scene_root_a = ['CZ09', 'CZ08', 'CZ03', 'CZ12', \
+                    'CZ10', 'CZ06', 'CZ07', 'CZ04', \
+                    'CZ13', 'CZ16', 'CZ02', 'CZ11', \
+                    'CZ14', 'CZ15', 'CZ05','CZ01']
+
+no_glare = [0, 0, 0, 0, 0]
 
 #SETUP
 
@@ -34,27 +37,23 @@ os.chdir(rad_dir)
 
 #MAIN
 
-glare_metrics_a = [['dgp', 'dgi', 'ugr', 'vcp', 'cgi']]
-no_glare = [0, 0, 0, 0, 0]
+for sky_scene_root in sky_scene_root_a:
+    for az_lbl in az_lbl_a:
+        for wwr in wwr_a:
 
-for az_lbl in az_lbls:
-    for wwr in wwrs:
+            #get the glare sensor position
+            sensr_spc = az_lbl + '_' + wwr
+            sensr_fn = [fn for fn in os.listdir('./views') if sensr_spc in fn and 'Bad_Glare_Snsr' in fn][0] #finds either vfh or vfv; doesn't matter.
 
-        #get the glare sensor position
-        sensr_spc = az_lbl + '_' + wwr
-        sensr_fn = [fn for fn in os.listdir('./views') if sensr_spc in fn and 'Bad_Glare_Snsr' in fn][0] #finds either vfh or vfv; doesn't matter.
+            with open('./views/' + sensr_fn) as f_r:
+                view = f_r.read().split(' ')
+            pos = ' '.join(view[2:14])
 
-        with open('./views/' + sensr_fn) as f_r:
-            view = f_r.read().split(' ')
-        pos = ' '.join(view[2:14])
+            az = float(az_lbl)
+            az_bounds = [az - 90, az + 90]
 
-        az = float(az_lbl)
-        az_bounds = [az - 90, az + 90]
-
-        for sky_scene_root in sky_scene_roots:
-
-            #only 1/2 of year for now to check resolution sensitivity. 
-            for hoy in range(1,4381):
+            glare_metrics_a = [['dgp', 'dgi', 'ugr', 'vcp', 'cgi']]
+            for hoy in range(1,8761):
                 dow = day_type_a[int(hoy/24) % 7]
 
                 sky_scene_cz_dir = sky_scene_dir + sky_scene_root + '-rad-skies/'
@@ -78,6 +77,7 @@ for az_lbl in az_lbls:
                     else:
                         glare_metrics = no_glare
                     glare_metrics_a.append(glare_metrics)
+                    print(str(hoy))
 
             with open('_'.join([sky_scene_root, sensr_spc]) + '_WN_UN_beam_glare_metrics.csv', 'w', newline='') as f_w:
                 csv.writer(f_w, dialect='excel').writerows(glare_metrics_a)
