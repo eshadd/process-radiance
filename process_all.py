c2fb2cbb39ed9e90c5ba640a63ec7f7612f58496
Eric Shadd <Eric Shadd>
2017-04-22 11:57:12 -0700

    finish

--- process_all.py
+++ process_all.py
@@ -1,22 +1,38 @@
 #pylint: skip-file
 
+import process_pat
+
 import os
 import csv
 
 #INPUT
 
-pat_path_a = ['C:/Test Lab/Clerestory 00 '+ format(cz,'02d') +'/' for cz in [4]]
-out_path = out_path = 'C:/Test Lab/Output/'
+pat_path_stem = 'E:/Fixed Slats 00 '
+out_path = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/Test Lab/'
+run_d = ['90 WN 90 WN']#, '10 3.500 25 1.750', '10 2.500 25 1.500', '10 2.000 25 1.375', '10 1.750 25 1.250', '15 2.500 30 1.500', '15 2.000 30 1.375', '15 1.750 30 1.250', '15 1.500 30 1.125', '20 2.125 35 1.375', '20 1.750 35 1.250', '20 1.500 35 1.125', '20 1.375 35 1.050', '25 1.750 40 1.250', '25 1.500 40 1.125', '25 1.375 40 1.050', '25 1.250 40 1.000']
+merge_dir = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/Test Lab/'
+merged_results_fn = 'merged_FS_results.csv'
 
-merge_dir = 'C:/Test Lab/Output/'
-merged_results_fn = 'merged_results.csv'
+# pat_path_stem = 'E:/Clerestory 00 '
+# out_path = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/Test Lab/'
+# run_d = ['90 CL 90 CL', '90 LL 90 LL']
+# merge_dir = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/Test Lab/'
+# merged_results_fn = 'merged_CL_results.csv'
+
+run_cz_a = [3, 4, 6, 7, 8, 9, 10, 12]
 
 #SETUP
 
-results_fn_a = [f for f in os.listdir(merge_dir) if f[-4:] == '.csv' and f != merged_results_fn]
+pat_path_a = [pat_path_stem + format(cz,'02d') +'/' for cz in run_cz_a]
+
 rad_dat_hdr = ['Weather', 'Case', 'Az', 'WWR', 'Bad Shade Hrs', 'Good Shade Hrs', 'Zone', 'Ctrl', 'Setpt', 'TDV']
 
 # MAIN
+
+results_fn_a = []
+for pat_path in pat_path_a:
+    results_fn = process_pat.process_pat(pat_path, run_d, out_path)
+    results_fn_a.append(results_fn)
 
 with open(merge_dir + merged_results_fn, 'w', newline='') as merged_results_fw:
     csv.writer(merged_results_fw, dialect='excel').writerow(rad_dat_hdr)
--- process_pat.py
+++ process_pat.py
@@ -8,56 +8,57 @@ import os
 
 #INPUT
 
-pat_path = 'C:/Test Lab/Clerestory 00 04/'
-out_path = 'C:/Test Lab/Output/'
-tdv_fref = 'C:/Test Lab/Input/2019 TDV Factors-V2-02.13.2017 - NRE30.csv'
+def process_pat(pat_path, run_d, out_path):
 
-#run_d = ['90 WN 90 WN', '10 3.500 25 1.750', '10 2.500 25 1.500', '10 2.000 25 1.375', '10 1.750 25 1.250', '15 2.500 30 1.500', '15 2.000 30 1.375', '15 1.750 30 1.250', '15 1.500 30 1.125', '20 2.125 35 1.375', '20 1.750 35 1.250', '20 1.500 35 1.125', '20 1.375 35 1.050', '25 1.750 40 1.250', '25 1.500 40 1.125', '25 1.375 40 1.050', '25 1.250 40 1.000']
-run_d = ['90 CL 90 CL', '90 LL 90 LL']
+    tdv_fref = 'C:/Determinant_J/Projects/T2419 CASE/Analysis/Input/2019 TDV Factors-V2-02.13.2017 - NRE30.csv'
 
-shade_case_d = {
-    'bad': {'threshold': 0.4, 'min_period': 21*24, 'check_times': [8], 'occ_hrs': [8, 17], \
-        'shaded': 0, 'shaded_hrs': 0, 'tot_shaded_hrs': 0, \
-        'unshd_run': 'NS', 'shd_run': 0.0}, 
-    'good': {'threshold': 0.6, 'min_period': 1, 'check_times': [8, 12], 'occ_hrs': [8, 17], \
-        'shaded': 0, 'shaded_hrs': 0, 'tot_shaded_hrs': 0, \
-        'unshd_run': 'NS', 'shd_run': 'DS'}
-    }
+    #TEMP
+    shade_case_d = {
+        'bad': {'threshold': 100, 'min_period': 21*24, 'check_times': [8], 'occ_hrs': [8, 17], \
+            'shaded': 0, 'shaded_hrs': 0, 'tot_shaded_hrs': 0, \
+            'unshd_run': 'NS', 'shd_run': 0.0}, 
+        'good': {'threshold': 100, 'min_period': 1, 'check_times': [8, 12], 'occ_hrs': [8, 17], \
+            'shaded': 0, 'shaded_hrs': 0, 'tot_shaded_hrs': 0, \
+            'unshd_run': 'NS', 'shd_run': 'DS'}
+        }
 
-setpt_a = [100,  200, 300, 500, 750, 1000]
+    setpt_a = [100,  200, 300, 500, 750, 1000]
 
-rad_dat_hdr = ['Weather', 'Case', 'Az', 'WWR', 'Bad Shade Hrs', 'Good Shade Hrs', 'Zone', 'Ctrl', 'Setpt', 'TDV']
+    rad_dat_hdr = ['Weather', 'Case', 'Az', 'WWR', 'Bad Shade Hrs', 'Good Shade Hrs', 'Zone', 'Ctrl', 'Setpt', 'TDV']
 
-pat_db = 'project.osp'
+    pat_db = 'project.osp'
 
-#SETUP
+    #SETUP
 
-case_d = {tc: {'none': tc + ' NS', 'good': tc + ' DS'} for tc in run_d}
+    case_d = {tc: {'none': tc + ' NS', 'good': tc + ' DS'} for tc in run_d}
 
-# Connect to the database file
-pat_db_conn = sqlite3.connect(pat_path + pat_db)
-pat_db_crsr = pat_db_conn.cursor()
+    # Connect to the database file
+    pat_db_conn = sqlite3.connect(pat_path + pat_db)
+    pat_db_crsr = pat_db_conn.cursor()
 
-pat_db_crsr.execute('SELECT name, directory FROM DataPointRecords')
-#note inconsitency fix: some no shade cases had 'NS', some didn't
-all_run_path_d = {rn if rn[-3]==' ' else rn + ' NS': rp for rn, rp in pat_db_crsr.fetchall()}
-run_path_d = {case: {'none': all_run_path_d[runs['none']], 'good': all_run_path_d[runs['good']]} for case, runs in case_d.items()}
+    pat_db_crsr.execute('SELECT name, directory FROM DataPointRecords')
+    #note inconsitency fix: some no shade cases had 'NS', some didn't
+    all_run_path_d = {rn if rn[-3]==' ' else rn + ' NS': rp for rn, rp in pat_db_crsr.fetchall()}
+    run_path_d = {case: {'none': all_run_path_d[runs['none']], 'good': all_run_path_d[runs['good']]} for case, runs in case_d.items()}
 
-pat_db_crsr.execute('SELECT weatherFileReferenceRecordId FROM AnalysisRecords')
-wthr_file_idx = pat_db_crsr.fetchone()[0]
-pat_db_crsr.execute('SELECT displayName FROM FileReferenceRecords WHERE id = {wfi}'.format(wfi=wthr_file_idx))
-wthr_fn = pat_db_crsr.fetchone()[0]
+    pat_db_crsr.execute('SELECT weatherFileReferenceRecordId FROM AnalysisRecords')
+    wthr_file_idx = pat_db_crsr.fetchone()[0]
+    pat_db_crsr.execute('SELECT displayName FROM FileReferenceRecords WHERE id = {wfi}'.format(wfi=wthr_file_idx))
+    wthr_fn = pat_db_crsr.fetchone()[0]
 
-pat_db_conn.close()
+    pat_db_conn.close()
 
-results_by_shd = {}
-for case, run_path_d in run_path_d.items():
-    rad_dat = distill_rad_data.run_distillr(case, setpt_a, shade_case_d, run_path_d, pat_path, wthr_fn, tdv_fref)
-    results_by_shd.setdefault(rad_dat[0], [rad_dat_hdr]).extend(rad_dat[1])
+    results_by_shd = {}
+    for case, run_path_d in run_path_d.items():
+        rad_dat = distill_rad_data.run_distillr(case, setpt_a, shade_case_d, run_path_d, pat_path, wthr_fn, tdv_fref)
+        results_by_shd.setdefault(rad_dat[0], [rad_dat_hdr]).extend(rad_dat[1])
 
-for cz, results in results_by_shd.items():
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
-    with open(out_path + format(cz, '02d') + '.csv', 'w', newline='') as f_w:
-        csv.writer(f_w, dialect='excel').writerows(results)
+    for cz, results in results_by_shd.items():
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+        results_fn = format(cz, '02d') + '.csv'
+        with open(out_path + results_fn, 'w', newline='') as f_w:
+            csv.writer(f_w, dialect='excel').writerows(results)
+            
+    return results_fn
 
